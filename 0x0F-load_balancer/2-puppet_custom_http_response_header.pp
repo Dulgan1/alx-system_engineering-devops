08cf173e2b97c3f6425fc1e1e4b33ae0666e2a93
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-04-08 06:16:25 -0700

    Initial commit

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,5 @@
+# Puppet: adds custom header X-Served-By
+exec { 'CustomHeader':
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx; sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By \$HOSTNAME;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+  provider => shell
+}
