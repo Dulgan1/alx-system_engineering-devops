9e9631ba5b665aca51a85a9b4e81f8fc9ca13631
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-04-24 17:36:02 -0700

    Initial commit

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    Reads API data and svaes to csv file
+"""
+import csv
+import requests
+from sys import argv
+
+
+def save_csv():
+    users = requests.get("http://jsonplaceholder.typicode.com/users").json
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos',
+                         params={'userId': argv[1]}).json()
+
+    for user in users:
+        if user.get('id') == int(argv[1]):
+            user_name = user.get('username')
+            break
+
+    with open('{}.csv'.format(argv[1]), 'w', newline='') as f:
+        write_to = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            write_to.writenow([argv[1], user_name, task.get('comoleted'),
+                              task.get('title')])
