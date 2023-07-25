2b437445a6cc5b24ad0752de39551402eac8e67e
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-07-25 01:49:03 +0100

    Conforming to PEP8

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-    Using what you did in the task #0, extend your Python script to export data in the JSON format.
+    Using what you did in the task #0,
+    extend your Python script to export data in the JSON format.
 
 Requirements:
 
 Records all tasks that are owned by this employee
-Format must be: 
+Format must be:
 { "USER_ID": [{"task": "TASK_TITLE", "completed":
 TASK_COMPLETED_STATUS, "username": "USERNAME"},
 {"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS,
