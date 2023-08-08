e276f32b24548108c1a5c8b7830660f6de5b73b3
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-08-08 23:08:15 +0100

    Initial commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Requests Reddit API for number of subscribers in a subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers={'User-Agent': 'Alx-Dulgan'})
+
+    data = response.json()
+    no_sub = data["data"]["subscribers"]
+
+    return no_sub
