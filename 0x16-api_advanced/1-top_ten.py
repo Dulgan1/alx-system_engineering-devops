82ec8aaf344553056006a14be58f3e3140f5465b
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-08-08 23:51:53 +0100

    Initial commit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Requests Reddit API for titles of 10 hot posts in a subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False,
+                            headers={'User-Agent': 'Alx-Dulgan'})
+
+    if response.status_code == 200:
+        data = response.json()
+        list_d = data["data"]["children"]
+        for d in list_d[:10]:
+            print(d["data"]["title"])
+
+    return 'None'
